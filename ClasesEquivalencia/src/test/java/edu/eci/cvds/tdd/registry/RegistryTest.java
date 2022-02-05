139b65ad4fcf836f7469467f2e19abf3243c3df1
unknown <andres.parra-q@labinfo.is.escuelaing.edu.co>
2022-02-05 09:53:05 -0500

    Pruebas Unitarias 1.0

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -8,9 +8,9 @@ public class RegistryTest {
     private Registry registry = new Registry();
 
     @Test
-    public void validateRegistryResult() {
+    public void validateRegistryValid() {
 
-        Person person = new Person();
+        Person person = new Person("Person00", 0000, 43, Gender.MALE, true);
 
         RegisterResult result = registry.registerVoter(person);
 
@@ -18,4 +18,41 @@ public class RegistryTest {
     }
 
     // TODO: Complete with more test cases
+    @Test
+    public void validateRegistryDead(){
+        Person person = new Person("Person01", 0001, 19, Gender.MALE, false);
+
+        RegisterResult result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.DEAD, result);
+
+    }
+
+    @Test
+    public void validateRegistryInvalidAge(){
+        Person person = new Person("Person02", 0002, -5, Gender.FEMALE, true);
+
+        RegisterResult result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+    }
+
+    @Test
+    public void validateRegistryUnderAge(){
+        Person person = new Person("Person03", 0003, 20, Gender.UNIDENTIFIED, true);
+
+        RegisterResult result = registry.registerVoter(person);
+
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+
+    @Test
+    public void validateRegistryDuplicate(){
+        Person person = new Person("Person04", 0004, 58, Gender.FEMALE, true);
+
+        registry.registerVoter(person);
+        RegisterResult result = registry.registerVoter(person);
+        
+        Assert.assertEquals(RegisterResult.DUPLICATED, result);
+    }
 }
